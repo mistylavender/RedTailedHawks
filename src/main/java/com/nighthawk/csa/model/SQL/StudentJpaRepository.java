1672220149eba667a355905db72ce72e525c61be
ridhimainukurti <rinukurti@gmail.com>
2022-05-19 08:30:11 -0700

    created the tables with and working on developing backend authentication/code.

--- src/main/java/com/nighthawk/csa/model/SQL/StudentJpaRepository.java
+++ src/main/java/com/nighthawk/csa/model/SQL/StudentJpaRepository.java
@@ -1,6 +1,9 @@
 package com.nighthawk.csa.model.SQL;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 /*
 Extends the JpaRepository interface from Spring Data JPA.
@@ -9,5 +12,13 @@ Extends the JpaRepository interface from Spring Data JPA.
 -- Via JPA the developer can retrieve data from relational databases to Java objects and vice versa.
  */
 public interface StudentJpaRepository extends JpaRepository<Student, Long> {
+    List<Student> findAllByOrderByName();
 
+    List<Student> findByNameContainingIgnoreCase(String firstname);
+
+
+    @Query(
+            value = "SELECT * FROM Student p WHERE p.firstname LIKE ?1",
+            nativeQuery = true)
+    List<Student> findByLikeTermNative (String term);
 }
